51bc8eac370db7ba9fbcae9303d831aee646ea75
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2015-05-08 13:57:01 -0500

    fixed indentation of spec file

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,7 +4,7 @@ describe 'Playing Game' do
 
 	it 'Changes marker on Board' do
 		@game = Game.new
-			@game.take_turn(1)
+		@game.take_turn(1)
 		expect(@game.board[0][0]).to eq('X')
 	end
 
@@ -17,56 +17,56 @@ describe 'Playing Game' do
 	end
 
 	it 'detects a win in a row' do
-			@game = Game.new
-			@game.take_turn(4)
-			@game.take_turn(1)
-			@game.take_turn(5)
-			@game.take_turn(2)
-			@game.take_turn(6)
+		@game = Game.new
+		@game.take_turn(4)
+		@game.take_turn(1)
+		@game.take_turn(5)
+		@game.take_turn(2)
+		@game.take_turn(6)
 		expect(@game.win).to eq(true)
 	end
 
 	it 'detects a win in a column' do
-			@game = Game.new
-			@game.take_turn(2)
-			@game.take_turn(1)
-			@game.take_turn(5)
-			@game.take_turn(9)
-			@game.take_turn(8)
+		@game = Game.new
+		@game.take_turn(2)
+		@game.take_turn(1)
+		@game.take_turn(5)
+		@game.take_turn(9)
+		@game.take_turn(8)
 		expect(@game.win).to be(true)
 	end
 
 	it 'detects a win in the first diagonal' do
-			@game = Game.new
-			@game.take_turn(1)
-			@game.take_turn(2)
-			@game.take_turn(5)
-			@game.take_turn(8)
-			@game.take_turn(9)
+		@game = Game.new
+		@game.take_turn(1)
+		@game.take_turn(2)
+		@game.take_turn(5)
+		@game.take_turn(8)
+		@game.take_turn(9)
 		expect(@game.win).to be(true)
 	end
 
 	it 'detects a win in the second diagonal' do
-			@game = Game.new
-			@game.take_turn(1)
-			@game.take_turn(2)
-			@game.take_turn(5)
-			@game.take_turn(8)
-			@game.take_turn(9)
+		@game = Game.new
+		@game.take_turn(1)
+		@game.take_turn(2)
+		@game.take_turn(5)
+		@game.take_turn(8)
+		@game.take_turn(9)
 		expect(@game.win).to be(true)
 	end
 
 	it 'detects a tie' do
-			@game = Game.new
-			@game.take_turn(1)
-			@game.take_turn(2)
-			@game.take_turn(3)
-			@game.take_turn(5)
-			@game.take_turn(4)
-			@game.take_turn(7)
-			@game.take_turn(6)
-			@game.take_turn(9)
-			@game.take_turn(8)
+		@game = Game.new
+		@game.take_turn(1)
+		@game.take_turn(2)
+		@game.take_turn(3)
+		@game.take_turn(5)
+		@game.take_turn(4)
+		@game.take_turn(7)
+		@game.take_turn(6)
+		@game.take_turn(9)
+		@game.take_turn(8)
 		expect(@game.win).to eq("tie")
 	end
 end
